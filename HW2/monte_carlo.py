d79310eda62acd7fc5a5a6cf3980a47faa3b0163
Auggie <thomas-augspurger@uiowa.edu>
2012-09-16 09:48:52 -0500

    some more files

---
+++ HW2/monte_carlo.py
@@ -0,0 +1,44 @@
+from __future__ import division
+import numpy as np
+from numpy import random
+from uniformRV import uniformRV
+import statsmodels.api as sm
+
+# To do: Calculate yHat (in or out of for loop?) and find s^2, the variance of predictions.
+def monte_carlo(x, N, form):
+    """
+    Estimates the coefficients from a simple regression between two vectors.
+    
+    Parameters
+    - x is the independent variable. e.g. a sample drawn from a uniform distribution
+    - N is the length of the resulting output vector. The number of iterations.  1000 in the HW.
+    - Form is the functional form for the draws of Y, perhaps conditional on x.  
+    Defined as an anonymous function.
+    
+    Returns: 
+        A flat np array containing each estimate of the intercept and slope 
+        on x from the (y, x) regression; the variance of b0 and b1 and the cov(b0, b1)
+        y is the mean of the n (= 100 in the Homework) predictions.
+    
+    Need to build in handling for if x has a constant column or not.
+    """
+    y = np.empty(len(x), dtype = float)
+    b = np.empty(5 * N, dtype = float)
+    b = b.reshape(N, 5)
+    ybar = np.empty(N, dtype = float)
+    mseModel = np.empty(N, dtype = float)
+    mseResiduals = np.empty(N)
+    
+    for j in range(0, N):
+        y[:] = form(x)
+        model = sm.OLS(y, x)
+        results = model.fit()
+        b[j, 0] = results.params[0]
+        b[j, 1] = results.params[1]
+        b[j, 2] = results.cov_params()[0, 0] # For beta 0 Non-robust.  Use results.HC0_se or 1, 2, 3.
+        b[j, 3] = results.cov_params()[1, 1] # For beta 1. Take sqrt of the std error.
+        b[j, 4] = results.cov_params()[0, 1] # For the cov(beta 0, beta 1)
+        ybar[j] = np.mean(y)
+        mseModel[j] = results.mse_model
+        mseResiduals[j] = results.mse_resid
+    return (b, ybar, mseModel, mseResiduals)
