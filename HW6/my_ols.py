ee9394922a117949eb16920f20a15befe5cd0a0d
Auggie <thomas-augspurger@uiowa.edu>
2012-10-28 10:44:33 -0500

    More of ASG's notation.

---
+++ HW6/my_ols.py
@@ -0,0 +1,51 @@
+from __future__ import division
+
+from scipy import linalg, c_, diagonal, dot
+from scipy import stats
+import numpy as np
+
+
+class my_ols:
+    def __init__(self, Y, X, Y_varname='Y', X_varnames='', add_constant=True):
+        self.Y = Y
+        if add_constant == True:
+            self.X = c_[np.ones(X.shape[0]), X]
+        else:
+            self.X = X
+        self.Y_varname = Y_varname
+        if not isinstance(X_varnames, list):
+            self.X_varnames = ['const'] + list(X_varnames)
+        else:
+            self.X_varnames = ['const'] + X_varnames
+
+        self.estimate()
+
+    def estimate(self):
+        self.nobs = self.Y.shape[0]
+        self.ncoef = self.X.shape[1]
+
+        self.Q = dot(self.X.T, self.X)
+        self.Q_inv = linalg.inv(self.X.T.dot(self.X))
+        self.A = linalg.inv(self.Q).dot(self.X.T)
+        self.N = self.X.dot(self.A)
+        self.M = np.eye(self.nobs) - self.N
+        XY = dot(self.X.T, self.Y)
+        self.b = dot(self.Q_inv, XY)
+
+        self.df_e = self.nobs - self.ncoef
+        self.df_r = self.ncoef - 1
+
+        self.e = self.Y - dot(self.X, self.b)
+        self.sse = dot(self.e, self.e) / self.df_e  # SSE
+        self.se = np.sqrt(diagonal(self.sse * self.Q_inv))  # Non robust SE
+        self.t = self.b / self.se
+
+        # TODO: make this take optional arg for one-sided vs. default of 2
+        self.p_value = (1 - stats.t.cdf(abs(self.t), self.df_e)) * 2
+        self.R2 = 1 - self.e.var() / self.Y.var()
+        self.R2adj = 1 - (1 - self.R2) * ((self.nobs - 1) / (self.nobs - self.ncoef))
+        self.F = (self.R2 / self.df_r) / ((1 - self.R2) / self.df_e)
+        self.F_p_value = 1 - stats.f.cdf(self.F, self.df_r, self.df_e)
+
+if __name__ == '__main__':
+    print 'hi'
