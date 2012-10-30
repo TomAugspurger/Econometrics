f0afa7c3ebdf50fb5055250bb1c7e1fc8cf518d5
Auggie <thomas-augspurger@uiowa.edu>
2012-10-29 19:45:42 -0500

    PEP-8.  moved f_test out.

--- HW6/using_my_ols.py
+++ HW6/using_my_ols.py
@@ -3,6 +3,7 @@ from __future__ import division
 
 import my_ols
 import pandas as pd
+import numpy as np
 
 # Read in
 df = pd.read_csv('CleanGreenData.csv', index_col=0, parse_dates=True)
@@ -12,9 +13,14 @@ exog = df[['constant', 'log_income_pp', 'log_gas_p', 'log_new_car_p',
 
 # Estimation
 model = my_ols.my_ols(endog, exog, Y_varname='log_gas_pp', X_varnames=[
-    'log_income_pp', 'log_gas_p', 'log_new_car_p', 'log_used_car_p'], add_constant=False)
+    'log_income_pp', 'log_gas_p', 'log_new_car_p',
+    'log_used_car_p'], add_constant=False)
 print model.b
 print model.t
 print model.p_value
 
 print model.R2
+
+R = np.array([[0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
+(f, p_value) = model.f_test(R)
+print f, p_value
