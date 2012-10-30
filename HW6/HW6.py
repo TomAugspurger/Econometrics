c9dc29882b03cb26c30eea05307ab65321820ca6
Auggie <thomas-augspurger@uiowa.edu>
2012-10-29 19:57:18 -0500

    PEP

--- HW6/HW6.py
+++ HW6/HW6.py
@@ -2,23 +2,13 @@ from __future__ import division
 
 import pandas as pd
 import numpy as np
-from scipy import linalg
 import statsmodels.api as sm
 
 # Read in
-df = pd.read_csv('greenDataFormat.csv', index_col=0, parse_dates=True)
-df.columns = ['gas', 'popn', 'gas_p', 'income', 'new_car_p',
-            'used_car_p', 'ppt', 'pd', 'pn', 'ps']
-df = df[['gas', 'popn', 'gas_p', 'income', 'new_car_p',
-            'used_car_p']]
-df = df[['gas', 'popn', 'gas_p', 'income', 'new_car_p', 'used_car_p']]
-endog = np.log(df.gas / df.popn)
-exog = pd.DataFrame(np.ones(len(df)))
-exog.index = df.index
-exog['log_income_pp'] = np.log(df.income / df.popn)
-exog['log_gas_p'] = np.log(df.gas_p)
-exog['log_new_car_p'] = np.log(df.new_car_p)
-exog['log_used_car_p'] = np.log(df.used_car_p)
+df = pd.read_csv('CleanGreenData.csv', index_col=0, parse_dates=True)
+endog = df['log_gas_pp']
+exog = df[['constant', 'log_income_pp', 'log_gas_p', 'log_new_car_p',
+       'log_used_car_p']]
 
 # With Statsmodels
 model = sm.OLS(endog, exog)
@@ -28,96 +18,5 @@ results.cov_HC0  # Robust Standard Error Matrix
 se = results.cov_params()  # Non-robust
 
 results.rsquared
-
-# Now by hand
-
-
-def coeff_est(endog, exog):
-    """
-    Estimate the coefficients of the regression equation, b,
-    using b = (X'X)^-1 * X'
-    """
-    Q = exog.T.dot(exog)
-    A = linalg.inv(Q).dot(exog.T)
-    b = A.dot(endog)
-    return b
-
-
-def std_errors(endog, exog):
-    """
-    Non robust standard errors. V(b) = sigma^2 Q^-1, assuming
-    homoskedasticity. ASG p. 167
-    """
-    n = len(endog)
-    k = len(exog.columns)
-    Q = exog.T.dot(exog)
-    A = linalg.inv(Q).dot(exog.T)
-    N = exog.values.dot(A)
-    M = np.eye(n) - N
-    e = M.dot(endog)
-    sigma_sq = e.T.dot(e) * (1 / (n - k))
-    return (sigma_sq, sigma_sq * linalg.inv(Q))
-
-
-def r_squared(endog, exog):
-    """
-    """
-    n = len(endog)
-    Q = exog.T.dot(exog)
-    A = linalg.inv(Q).dot(exog.T)
-    N = exog.values.dot(A)
-    M = np.eye(n) - N
-    e = M.dot(endog)
-
-    num = e.T.dot(e)
-    M0 = np.eye(n) - (1 / n) * np.ones([n, n])
-    denom = endog.T.dot(M0).dot(endog)
-    r_sqr = 1 - num / denom
-    return r_sqr
-
-
-def helper(endog, exog):
-    """
-    TODO: add check for ones
-    """
-    n = len(endog)
-    k = len(exog.columns)
-    Q = exog.T.dot(exog)
-    A = linalg.inv(Q).dot(exog.T)
-    N = exog.values.dot(A)
-    M = np.eye(n) - N
-    e = M.dot(endog)
-    N = exog.values.dot(A)
-    M = np.eye(n) - N
-    num = e.T.dot(e)
-    M0 = np.eye(n) - (1 / n) * np.ones([n, n])
-    denom = endog.T.dot(M0).dot(endog)
-    return n, k, Q, A, N, M, e, N, M, num, denom
-
-
-def coeff_est(endog, A):
-    """
-    Estimate the coefficients of the regression equation, b,
-    using b = (X'X)^-1 * X'
-    """
-    b = A.dot(endog)
-    return b
-
-
-def std_errors(e, n, k, Q):
-    """
-    Non robust standard errors. V(b) = sigma^2 Q^-1, assuming
-    homoskedasticity. ASG p. 167
-    """
-    sigma_sq = e.T.dot(e) * (1 / (n - k))
-    std_err  = sigma_sq * linalg.inv(Q)
-    return (sigma_sq, std_err)
-
-
-def r_squared(num, denom):
-    """
-    """
-    r_sqr = 1 - num / denom
-    return r_sqr
-
-
+R = np.array([[0, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
+results.f_test(R)
