72d7dd865d3cf1b2afb04ae690a3d2bb5de7e65d
Auggie <thomas-augspurger@uiowa.edu>
2012-11-15 09:30:16 -0600

    2 exogenous params so no plotting.  Removed need for to dfs.

--- HW7/HW7_part3_qr.R
+++ HW7/HW7_part3_qr.R
@@ -3,10 +3,6 @@ data = read.csv("clean_growth.csv")
 # Regessions: growth on
 # 1: tradeshare and yearsschool
 
-endog = data[["growth"]]
-
-exog = data[,c('tradeshare', 'yearsschool')]
-q1 <- rq(endog ~ tradeshare + yearsschool, tau=.5, exog)
+q1 <- rq(growth ~ tradeshare + yearsschool, tau=.5, data)
 
 summary(q1)
-plot(q1)
