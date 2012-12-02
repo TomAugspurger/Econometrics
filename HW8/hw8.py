f12804affff3ba14a8385ea7717b843b5d6d8522
Auggie <thomas-augspurger@uiowa.edu>
2012-12-02 14:50:21 -0600

    First commit for HW8 part 4

--- HW8/hw8.py
+++ HW8/hw8.py
@@ -18,3 +18,43 @@ exog_a = sm.add_constant(exog_a, prepend=True)
 endog_a.head()
 exog_a.head()
 
+model_a = sm.OLS(endog_a, exog_a)
+results_a = model_a.fit()
+
+print(results_a.summary())
+
+#b: educ on exog_a - educ + nearc4
+endog_b = df['educ']
+exog_b = exog_a.drop('educ', axis=1).join(df['nearc4'])
+
+model_b = sm.OLS(endog_b, exog_b)
+results_b = model_b.fit()
+
+# Part c: IV lwage on explanatory w/ nearc4 as instr. for educ.
+endog_x = df['educ']
+exog_x = df[['const', 'nearc4', 'exper', 'expersq', 'black', 'south',
+        'reg661', 'reg662', 'reg663', 'reg664', 'reg665',
+        'reg666', 'reg667', 'reg668', 'smsa66']]
+
+
+model_x = sm.OLS(endog_x, exog_x)
+results_x = model_x.fit()
+df['educ_hats'] = results_x.fittedvalues
+
+endog_c = df['lwage']
+
+# replace educ with IV.
+exog_c = df[['const', 'educ_hats' 'exper', 'expersq', 'black', 'south',
+        'reg661', 'reg662', 'reg663', 'reg664', 'reg665',
+        'reg666', 'reg667', 'reg668', 'smsa66']]
+
+model_c = sm.OLS(endog_c, exog_c)
+results_c = model_c.fit()
+
+# Need to compare the widths of the two intervals on Beta_educ.
+
+##########################
+#### PART D
+##########################
+# Reuse endog_x from c;
+exog_x2 = df[['const', 'nearc2', 'nearc4']]
